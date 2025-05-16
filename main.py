88818453f99820730459303bbe90566d5e66893a
candog85 <candog851@gmail.com>
2025-05-16 09:16:06 -0400

    yaya

--- main.py
+++ main.py
@@ -65,7 +65,6 @@ class User:
     def get_id(self):
         return str(self.id)
     
-
 # Load User Session
 @login_manager.user_loader
 def load_user(id):
@@ -235,9 +234,19 @@ def browse(page):
         
         """)
     
-    length=math.ceil((len(cursor.fetchall()))/20)
+    length=math.ceil((len(cursor.fetchall()))/16)
     
+
+    if page<4:
+        page_range=[1,6]
+        if page_range[1]>length:
+            page_range[1]=length+1
     
+    elif page>=3:
+        page_range=[page-2, page+3]
+        if page+1>=length:
+            page_range=[length-4, length+1]
+            
     
     cursor.execute(f"""
                    
@@ -250,7 +259,7 @@ def browse(page):
     cursor.close()
     conn.close()
     
-    return render_template("browse.html.jinja", colleges=colleges, page=page, query=query, length=length)
+    return render_template("browse.html.jinja", colleges=colleges, page=page, query=query, length=length, page_range=page_range)
     # Note: For now, the database connection and data fetcher are placeholders. This WILL be changed later as neccessary.  
 
 # Search Colleges
@@ -639,12 +648,14 @@ def college(college_id):
     
     cursor.execute(f"""
                    
-    SELECT `page` from `User`
+    SELECT * from `User`
     WHERE `id` = %s               
                    
                    """,(customer_id))
     
-    page=cursor.fetchone()['page']
+    student=cursor.fetchone()
+    
+    page=student['page']
     
     cursor.execute(f"""
                    
@@ -825,7 +836,6 @@ def update():
         flash("One or more of your fields are invalid!", 'error')
     
     return redirect("/analytics")
-
 
 # Update user settings
 @app.route("/settings/update_user", methods=["POST"])
